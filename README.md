e2ffab8b857d9e6c0a5fa951f7de3b4fec07babe
Joshua Holbrook <josh.holbrook@gmail.com>
2012-02-10 15:17:23 -0800

    Added readme, etc.

---
+++ README.md
@@ -0,0 +1,47 @@
+# equip
+
+** Equip connect middlewares to your flatiron stack. **
+
+## Flatiron plugin example:
+
+```js
+var flatiron = require('flatiron'),
+    app = flatiron.app;
+
+var middleware = function oilpan (req, res, next) {
+  res.writeHead(500, { 'content-type': 'text/plain' });
+  res.end('This request "fell through" the middleware stack!');
+}
+
+app.use(flatiron.plugins.http);
+app.use(equip, middleware);
+
+app.start(8080);
+```
+
+## Alternate use with flatiron:
+
+```js
+app.use(equip(middleware));
+```
+
+## Alternate use with director:
+
+```js
+
+app.router.get('*', equip(middleware));
+```
+
+## Or, use it like a regular middleware:
+
+```js
+
+var express = require('express');
+
+// Still works.
+express.use(equip(middleware));
+```
+
+# Tests?
+
+LATER. But they will come. I want this for a bunch of things.
--- index.js
+++ index.js
@@ -1,33 +1,44 @@
-function createBroomstick (middleware) {
-  var self = this || {};
+//
+// index.js
+//
+// This is a module that helps wrap connect-style middlewares for compatibility
+// with flatiron, particularly for use with director and with 
+// `flatiron.plugins.http`.
+//
 
-  return function broomstick (req, res, next) {
+// This function can be used as an .attach method for broadway deliciousness.
+function equip (middleware) {
+  if (!this.http) {
+    throw new Error('You are trying to equip a middleware without the http plugin!');
+  }
+  else {
+    // Let's just add the middleware to the "before" middleware stack.
+    this.http.before.concat(middleware);
+  }
+}
+
+
+// Call the export as a function to create a broomstick-like director route
+// that falls back to middleware mode barring the existence of `this.req` and
+// `this.res`;
+var plugin = module.exports = function createBroomstick (middleware) {
+
+  var self = this || {},
+      broomstick = function broomstick (req, res, next) {
 
     // Deal with the case where we're using director.
     if (self.req && self.res) {
       return middleware(self.req, self.res, function () { self.res.emit('next'); });
     }
     else {
+      // Use the regular middleware otherwise.
       return middleware(req, res, next);
     }
   };
+
+  // 
+  broomstick.attach = equip;
 }
 
-function createPlugin () {
-  var plugin = createBroomstick;
- 
-  plugin.attach = function (middleware) {
-    if (!this.http) {
-      throw new Error('You are trying to equip a middleware without the http plugin!');
-    }
-    else {
-      // Let's just add the middleware to the "before" middleware stack.
-      this.http.before.concat(middleware);
-    }
-  }
-
-  return plugin;
-
-};
-
-module.exports = createPlugin();
+// Also expose an "attach" plugin that will dump it into your middleware stack.
+plugin.attach = equip;
